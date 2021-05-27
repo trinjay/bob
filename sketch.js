28101741b352897259042c55eb180e5939f5815e
trinjay <75678794+trinjay@users.noreply.github.com>
2021-05-27 10:52:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,46 @@
-
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
-const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
-var rope1,rope2,rope3, rope4,rope5;
-var world;
-
-
+var car,wall;
+var speed, weight; 
 function setup() {
-	createCanvas(800, 600);
-	rectMode(CENTER);
+  createCanvas(1600, 400);
 
 
-	engine = Engine.create();
-	world = engine.world;
+	speed=random(55,90)
+	weight=random(400,1500)
 
-	roofObject=new roof(400,250,230,20);
-	bob1 = new bob(320,575,40)
-	bob2 = new bob(360,575,40)
-	bob3 = new bob(420,575,40)
-	bob4 = new bob(460,575,40)
-	bob5 = new bob(520,575,40)
-	
-	rope1=new rope(bob1.body,roofObject.body,-80, 0)
-	rope2=new rope(bob2.body,roofObject.body,-40, 0)
-	rope3=new rope(bob3.body,roofObject.body,1,0)
-	rope4=new rope(bob4.body,roofObject.body,40,0)
-	rope5=new rope(bob5.body,roofObject.body,80,0)
-	Engine.run(engine);
-	
-  
+
+	car=createSprite(50, 200, 50,50);   
+
+	car.velocityX = speed;
+
+	car.shapeColor=color(255);
+  	wall=createSprite(1500,200, 60, height/2)
+  	wall.shapeColor=color(80,80,80)
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(230);
-  roofObject.display();
+  background(0);
 
-  rope1.display();
-  rope2.display();
-rope3.display();
-rope4.display();
-rope5.display();
-  bob1.display();
-  bob2.display();
-  bob3.display();
-  bob4.display();
-  bob5.display();
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22500;
+	if(deformation>180)
+	{
+		car.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		car.shapeColor=color(0,255,0);
+	}
+  }  
+  
+  drawSprites();
  
 }
-
-
-
-
-
-
